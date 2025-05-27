1c5dcaca344c6f091721394fb7bcd52568492a21
Filson <ajffilson@gmail.com>
2025-05-27 16:21:06 +0100

    Suspense change

---
+++ src/app/sign-in/[[...sign-in]]/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ReactNode, Suspense } from "react";
+
+export default function LoginLayout({ children }: { children: ReactNode }) {
+    return (
+        <Suspense
+            fallback={
+                <div className="flex items-center justify-center h-screen">
+                    Loading…
+                </div>
+            }
+        >
+            {children}
+        </Suspense>
+    );
+}
